6eb5bf81136eb27e752614da7c8280f307594e19
Niklas Keller <me@kelunik.com>
2019-04-02 20:34:58 +0200

    Optimize and fix Tunnel
    Previously, a concurrent write and read at the beginning didn't behave as intended.

--- src/Tunnel.php
+++ src/Tunnel.php
@@ -7,9 +7,10 @@ use Amp\ByteStream\OutputStream;
 use Amp\Promise;
 use Amp\Ssh\Channel\ChannelInputStream;
 use Amp\Ssh\Channel\ChannelOutputStream;
-use Amp\Success;
+use function Amp\call;
 
-class Tunnel implements InputStream, OutputStream {
+class Tunnel implements InputStream, OutputStream
+{
     /** @var Channel\DirectTcpIp */
     private $directTcpIp;
 
@@ -19,45 +20,77 @@ class Tunnel implements InputStream, OutputStream {
     /** @var ChannelOutputStream */
     private $output;
 
+    /** @var Promise|null */
+    private $connectPromise;
+
+    /** @var bool */
     private $connected = false;
 
-    public function __construct(SshResource $sshResource, string $host, int $port, string $originHost, string $originPort) {
+    public function __construct(
+        SshResource $sshResource,
+        string $host,
+        int $port,
+        string $originHost,
+        string $originPort
+    ) {
         $this->directTcpIp = $sshResource->createDirectTcpIp($host, $port, $originHost, $originPort);
         $this->input = new ChannelInputStream($this->directTcpIp->getDataEmitter()->iterate());
         $this->output = new ChannelOutputStream($this->directTcpIp);
     }
 
-    public function read(): Promise {
-        return \Amp\call(function () {
+    public function read(): Promise
+    {
+        if ($this->connected) {
+            return $this->input->read();
+        }
+
+        return call(function () {
             yield $this->connect();
 
             return yield $this->input->read();
         });
     }
 
-    public function write(string $data): Promise {
-        return \Amp\call(function () use ($data) {
+    private function connect(): Promise
+    {
+        if ($this->connectPromise) {
+            return $this->connectPromise;
+        }
+
+        $connected = &$this->connected;
+        $this->connectPromise = $this->directTcpIp->open();
+        $this->connectPromise->onResolve(static function ($e) use (&$connected) {
+            if (!$e) {
+                $connected = true;
+            }
+        });
+
+        return $this->connectPromise;
+    }
+
+    public function write(string $data): Promise
+    {
+        if ($this->connected) {
+            return $this->output->write($data);
+        }
+
+        return call(function () use ($data) {
             yield $this->connect();
 
             return $this->output->write($data);
         });
     }
 
-    public function end(string $finalData = ''): Promise {
-        return \Amp\call(function () use ($finalData) {
+    public function end(string $finalData = ''): Promise
+    {
+        if ($this->connected) {
+            return $this->output->end($finalData);
+        }
+
+        return call(function () use ($finalData) {
             yield $this->connect();
 
             return $this->output->end($finalData);
         });
-    }
-
-    private function connect(): Promise {
-        if ($this->connected) {
-            return new Success();
-        }
-
-        $this->connected = true;
-
-        return $this->directTcpIp->open();
     }
 }
